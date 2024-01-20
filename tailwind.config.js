93ce78124ceec0b94b68012be2c0d8b4373233fa
Jahanara Akter <jahanaraaktersuma100@gmail.com>
2024-01-20 18:26:46 +0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        'Bebas': ['"Bebas Neue"'],
+        'Roboto': ['"Roboto"'],
+      },
+    },
+  },
+  plugins: [],
+}
